25f4acc29eb59ec9168247311bb2456f66553c61
Eric Laurello <eric.laurello@flipsidecrypto.com>
2025-07-14 17:01:32 -0400

    .

--- models/streamline/core/realtime/streamline__checkpoints_realtime.sql
+++ models/streamline/core/realtime/streamline__checkpoints_realtime.sql
@@ -4,7 +4,7 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"checkpoints",
-        "sql_limit" :"800000",
+        "sql_limit" :"5000000",
         "producer_batch_size" :"200000",
         "worker_batch_size" :"50000",
         "async_concurrent_requests" :"25",
