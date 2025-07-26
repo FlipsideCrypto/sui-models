ddbbf97be0c02a4842c5ce85842be39f9da733f9
Eric Laurello <eric.laurello@flipsidecrypto.com>
2025-07-26 08:42:32 -0400

    back to 40

--- models/streamline/core/realtime/streamline__transactions_realtime.sql
+++ models/streamline/core/realtime/streamline__transactions_realtime.sql
@@ -6,8 +6,8 @@
         params ={ "external_table" :"transactions",
         "sql_limit" :"1000000",
         "producer_batch_size" :"100000",
-        "worker_batch_size" :"2000",
-        "async_concurrent_requests" :"7",
+        "worker_batch_size" :"2500",
+        "async_concurrent_requests" :"10",
         "sql_source" :"{{this.identifier}}",
         'exploded_key': '["result"]',
         "order_by_column": "checkpoint_number DESC" }
